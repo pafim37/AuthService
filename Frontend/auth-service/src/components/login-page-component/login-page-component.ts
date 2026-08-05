59cc592fc37dca4875029f120d29dccfed96edc7
pafim37 <astropawelpiatek@gmail.com>
2026-08-05 18:30:45 +0200

    feat: style login page

--- Frontend/auth-service/src/components/login-page-component/login-page-component.ts
+++ Frontend/auth-service/src/components/login-page-component/login-page-component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-page-component',
-  imports: [MatLabel, MatFormField],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatCardHeader,
+    MatCardSubtitle,
+    MatCardTitle,
+    MatFormField,
+    MatInput,
+    MatLabel,
+  ],
   templateUrl: './login-page-component.html',
   styleUrl: './login-page-component.css',
 })
